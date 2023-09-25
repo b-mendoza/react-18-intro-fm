bbfe9a76e556f25129664c96332b760db81d0a50
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-09-24 23:37:23 -0600

    chore: add Prettier setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  plugins: [
+    './node_modules/prettier-plugin-jsdoc/dist/index.js',
+    'prettier-plugin-organize-imports',
+  ],
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+export default prettierConfig;
