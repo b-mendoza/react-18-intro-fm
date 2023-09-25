7501ebfa884b0b80cf80c77f2cacd5233b5716a1
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-09-25 00:30:39 -0600

    chore: add `react` ESLint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,10 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/strict',
   ],
   ignorePatterns: ['!.*', '*.tsbuildinfo', 'dist', 'node_modules'],
   parser: '@typescript-eslint/parser',
