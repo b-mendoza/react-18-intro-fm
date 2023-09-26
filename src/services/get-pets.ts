8a31e0c97fe14670fb3c738abdf5adc811a96e2d
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-09-26 00:03:04 -0600

    feat: add `get-pets` service

---
+++ src/services/get-pets.ts
@@ -0,0 +1,29 @@
+import { type AllowedAnimal } from '../constants/animals.ts';
+import { apiResponseSchema } from '../constants/schemas.ts';
+import { API_URL } from '../constants/shared.ts';
+
+type GetPetsArgs = Partial<{
+  animal: AllowedAnimal;
+  breed: string;
+  location: string;
+}>;
+
+export const getPets = async (args: GetPetsArgs) => {
+  const { animal, breed, location } = args;
+
+  const apiURL = new URL(API_URL);
+
+  Object.entries({ animal, breed, location }).forEach(([key, value]) => {
+    if (value == null) return;
+
+    apiURL.searchParams.append(key, value);
+  });
+
+  const response = await fetch(apiURL);
+
+  const data = await response.json();
+
+  return apiResponseSchema.parse(data);
+};
+
+export type GetPets = Awaited<ReturnType<typeof getPets>>;
