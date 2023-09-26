89c686e931a041d4e3d815764bf8b8e3f76823d0
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-09-25 23:43:25 -0600

    refactor: update `App` component

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,27 +1,6 @@
-import { nanoid } from 'nanoid';
+import { Toaster } from 'react-hot-toast';
 
-import { Pet } from '../pet';
-
-const animals = [
-  {
-    animal: 'Dog',
-    breed: 'Havanese',
-    id: nanoid(),
-    name: 'Luna',
-  },
-  {
-    animal: 'Bird',
-    breed: 'Cockatiel',
-    id: nanoid(),
-    name: 'Pepper',
-  },
-  {
-    animal: 'Cat',
-    breed: 'Mixed',
-    id: nanoid(),
-    name: 'Doink',
-  },
-] as const;
+import { SearchParams } from '../search-params';
 
 type AppProps = React.HTMLProps<HTMLDivElement>;
 
@@ -30,9 +9,9 @@ export const App = (props: AppProps) => {
     <div {...props}>
       <h1>Adopt Me!</h1>
 
-      {animals.map((animal) => {
-        return <Pet key={animal.id} {...animal} />;
-      })}
+      <SearchParams />
+
+      <Toaster />
     </div>
   );
 };
